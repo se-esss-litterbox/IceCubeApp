cab47e5ae4cb514cac37230d5ea561083a3586d2
stevemolloy <stephen.molloy@ice-tech.systems>
2016-04-10 15:40:16 +0200

    User sign-in and sign-out works now

--- toplevel.go
+++ toplevel.go
@@ -3,23 +3,47 @@ package icecubeapp
 import (
 	"fmt"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
 	http.HandleFunc("/", welcome)
+	http.HandleFunc("/signedout", signedout)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, testingForm)
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	url, _ := user.LogoutURL(c, "/signedout")
+	fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
 }
 
-const testingForm = `
+func signedout(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, signedoutForm)
+}
+
+const signedoutForm = `
 <html>
   <head>
     <title>IceCube App</title>
   </head>
   <body>
-    <p>Hello world!</p>
+    <p>Thanks for visiting!</p>
+    <a href="/">Sign in again</a>
   </body>
 </html>
 `
