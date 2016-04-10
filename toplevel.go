09d99a054a6d05d94bf4e3c5c1d017bd3043e44e
stevemolloy <stephen.molloy@ice-tech.systems>
2016-04-10 15:11:35 +0200

    Hello world :)

--- toplevel.go
+++ toplevel.go
@@ -1,5 +1,25 @@
 package icecubeapp
 
-func init() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func init() {
+	http.HandleFunc("/", welcome)
 }
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, testingForm)
+}
+
+const testingForm = `
+<html>
+  <head>
+    <title>IceCube App</title>
+  </head>
+  <body>
+    <p>Hello world!</p>
+  </body>
+</html>
+`
